34dcc48c97c77e1279bd6bb9cde2b717d5a7ba65
Mikiko9882 <teddy.0802.t@gmail.com>
2024-02-04 01:42:39 +0900

    フォロー、フォロワーボタン

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_03_124456) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_03_163216) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "test_results", force: :cascade do |t|
     t.string "test_name", null: false
