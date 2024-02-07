bb4e7a894d7039cae56eca9ee3208795bea1b702
Mikiko9882 <teddy.0802.t@gmail.com>
2024-02-07 11:18:16 +0900

    ボタンまで

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -3,11 +3,22 @@ class RelationshipsController < ApplicationController
   def create
     @user = User.find(params[:user_id])
     current_user.follow(@user)
+    render turbo_stream: turbo_stream.replace(
+      "follow-ajax-#{@user.id}",
+      partial: 'relationships/btn',
+      locals: { user: @user, followed: true },
+    )
   end
   
   def destroy
     @user = User.find(params[:user_id])
     current_user.unfollow(@user)
+    render turbo_stream: turbo_stream.replace(
+      "follow-ajax-#{@user.id}",
+      partial: 'relationships/btn',
+      locals: { user: @user, followed: false },
+    )
+
   end
   
   def followings
